5d9a4a5ef741a4bf64edef0c7c62497d207d6291
Daniel Andrlik <daniel@andrlik.org>
2018-08-29 17:18:44 -0400

    🚀 Update utility sync script to reduce errors.

--- utility/sync_pipenv_to_req.sh
+++ utility/sync_pipenv_to_req.sh
@@ -8,6 +8,7 @@ where:
      -h/--help Show this help text
      -p FILENAME Specify what file should be used for production packages (default: requirements.txt)
      -d FILENAME Specify what file should be used for development packages (default: test_requirements.txt)
+     -D Skip creating file of dev/test requirements
      -l Lock/pin the versions (note, VCS packages will always be pinned.)
 "
 lock=false
@@ -25,6 +26,9 @@ do
             ;;
         -d )
             DEV_FILE="$2"
+            ;;
+        -D )
+            DEV_FILE=""
             ;;
         -l )
             lock=true
@@ -53,27 +57,31 @@ fi
 echo "Syncing production requirement packages..."
 if [ "$lock" = true ];then
     echo "Pinning requirements..."
-    pipenv lock --requirements | grep -v "#" | cut -d' ' -f1 > $PRODUCTION_FILE
+    pipenv lock --requirements | grep -v "#" | cut -d' ' -f1 | grep -v "^\-i$" > $PRODUCTION_FILE
 else
-    pipenv lock --requirements | grep -v "#" | sed 's/==.*//' > $PRODUCTION_FILE
+    pipenv lock --requirements | grep -v "#" | sed 's/==.*//' | grep -v "^\-i " > $PRODUCTION_FILE
 fi
 echo "Syncing production VCS packages..."
 pipenv lock --requirements | grep "#" | sed 's/# //' >> $PRODUCTION_FILE
 echo "Finished production packages!"
 
 echo "--------------"
-if [ "$DEV_FILE" != "test_requirements.txt" ]; then
-    echo "Using development file of ${DEV_FILE}."
-fi
-echo "Syncing dev requirement packages..."
-if [ "$lock" = true ];then
-    echo "Pinning requirements..."
-    pipenv lock --dev --requirements | grep -v "#" | cut -d' ' -f1 > $DEV_FILE
+if [ ! $DEV_FILE ]; then
+    echo "Skipping development file."
 else
-    pipenv lock --dev --requirements | grep -v "#" | sed 's/==.*//' > $DEV_FILE
+    if [ "$DEV_FILE" != "test_requirements.txt" ]; then
+        echo "Using development file of ${DEV_FILE}."
+    fi
+    echo "Syncing dev requirement packages..."
+    if [ "$lock" = true ];then
+        echo "Pinning requirements..."
+        pipenv lock --dev --requirements | grep -v "#" | cut -d' ' -f1 | grep -v "^\-i$" > $DEV_FILE
+    else
+        pipenv lock --dev --requirements | grep -v "#" | sed 's/==.*//' | grep -v "^\-i " > $DEV_FILE
+    fi
+    echo "Syncing dev VCS packages..."
+    pipenv lock --dev --requirements | grep "#" | sed 's/# //' >> $DEV_FILE
+    echo "Finished dev packages!"
 fi
-echo "Syncing dev VCS packages..."
-pipenv lock --dev --requirements | grep "#" | sed 's/# //' >> $DEV_FILE
-echo "Finished dev packages!"
 echo "--------------"
 echo "Done!"
