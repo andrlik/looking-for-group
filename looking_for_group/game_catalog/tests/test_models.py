a65bc4998a230ac7cc5760100dbdcd853e7639dc
Daniel Andrlik <daniel@andrlik.org>
2018-09-01 09:42:40 -0400

    ✅ 🚑 💄 Fix tests for tag inheritance and update catalog detail views.
    Detail views now display both images, official links, and required disclaimers.

--- looking_for_group/game_catalog/tests/test_models.py
+++ looking_for_group/game_catalog/tests/test_models.py
@@ -26,32 +26,32 @@ class TestTagInheritance(TestCase):
 
     def test_inherited_tags_base_calculation(self):
         assert self.system.tags.count() == 2
-        assert not self.system.inherited_tags
+        assert len(self.system.inherited_tags) == self.system.tags.count()
         assert self.game.tags.count() == 1
-        assert len(self.game.inherited_tags) == 2
+        assert len(self.game.inherited_tags) == 3
         assert self.module.tags.count() == 1
-        assert len(self.module.inherited_tags) == 3
+        assert len(self.module.inherited_tags) == 4
 
     def test_add_tag_to_parent(self):
         self.system.tags.add("fantasy")
-        assert len(PublishedGame.objects.get(pk=self.game.pk).inherited_tags) == 3
-        assert len(PublishedModule.objects.get(pk=self.module.pk).inherited_tags) == 4
+        assert len(PublishedGame.objects.get(pk=self.game.pk).inherited_tags) == 4
+        assert len(PublishedModule.objects.get(pk=self.module.pk).inherited_tags) == 5
 
     def test_remove_tags_from_parent(self):
-        assert len(PublishedGame.objects.get(pk=self.game.pk).inherited_tags) == 2
+        assert len(PublishedGame.objects.get(pk=self.game.pk).inherited_tags) == 3
         self.system.tags.remove("monkey")
+        assert len(PublishedGame.objects.get(pk=self.game.pk).inherited_tags) == 2
+        assert (
+            len(PublishedModule.objects.get(pk=self.module.pk).inherited_tags) == 3
+        )
+
+    def test_remove_all_tags_from_parent(self):
+        self.system.tags.clear()
         assert len(PublishedGame.objects.get(pk=self.game.pk).inherited_tags) == 1
         assert (
             len(PublishedModule.objects.get(pk=self.module.pk).inherited_tags) == 2
         )
 
-    def test_remove_all_tags_from_parent(self):
-        self.system.tags.clear()
-        assert not PublishedGame.objects.get(pk=self.game.pk).inherited_tags
-        assert (
-            len(PublishedModule.objects.get(pk=self.module.pk).inherited_tags) == 1
-        )
-
     def test_tag_names_only(self):
-        tag_names = self.game.inherited_tag_names
-        assert tag_names == ['weird', 'monkey']
+        tag_names = sorted(self.game.inherited_tag_names)
+        assert tag_names == ['discovery', 'monkey', 'weird']
