1bf87af050cda663cc6bc0b7d972f5d00885af36
Daniel Andrlik <daniel@andrlik.org>
2018-09-02 14:41:49 -0400

    change newrelic entrypoint.

--- config/wsgi.py
+++ config/wsgi.py
@@ -15,6 +15,7 @@ framework.
 """
 import os
 import sys
+import newrelic.agent
 
 from django.core.wsgi import get_wsgi_application
 
@@ -36,7 +37,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # setting points here.
 application = get_wsgi_application()
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-    application = Sentry(application)
+    application = newrelic.agent.WSGIApplicationWrapper(Sentry(application))
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
