0090e7c49747c91bc3036741860c45c0c29d050d
Daniel Andrlik <daniel@andrlik.org>
2018-09-02 14:57:33 -0400

    remove new relic

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: bin/start-nginx bin/start-pgbouncer python config/run.py
-worker: bin/start-pgbouncer newrelic-admin run-program python manage.py qcluster
+worker: bin/start-pgbouncer python manage.py qcluster
 
--- config/wsgi.py
+++ config/wsgi.py
@@ -15,7 +15,6 @@ framework.
 """
 import os
 import sys
-import newrelic.agent
 
 from django.core.wsgi import get_wsgi_application
 
@@ -37,7 +36,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # setting points here.
 application = get_wsgi_application()
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-    application = newrelic.agent.WSGIApplicationWrapper(Sentry(application))
+    application = Sentry(application)
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
