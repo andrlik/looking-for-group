a873ed6ac118768047d3e31548f1a3be275e4415
Daniel Andrlik <daniel@andrlik.org>
2018-09-05 08:22:00 -0500

    🚧 💄 Working on new dashboard style layout design.

--- looking_for_group/static/js/project.js
+++ looking_for_group/static/js/project.js
@@ -1,1 +1,5 @@
 /* Project specific Javascript goes here. */
+$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+    e.preventDefault();
+    $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+});
