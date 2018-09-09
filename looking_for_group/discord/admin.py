8a023f37483776e64593de4c9d1b13b071e0220a
Daniel Andrlik <daniel@andrlik.org>
2018-09-09 17:57:01 -0400

    A true production fix

--- looking_for_group/discord/admin.py
+++ looking_for_group/discord/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class ServerAdmin(admin.ModelAdmin):
+    pass
+
+class GamerLinkAdmin(admin.ModelAdmin):
+    pass
+
+class CommLinkAdmin(admin.ModelAdmin):
+    pass
+
+class ServerMembership(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.DiscordServer, ServerAdmin)
+admin.site.register(models.GamerDiscordLink, GamerLinkAdmin)
+admin.site.register(models.CommunityDiscordLink, CommLinkAdmin)
+admin.site.register(models.DiscordServerMembership, ServerMembership)
