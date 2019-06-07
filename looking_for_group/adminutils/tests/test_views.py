e425d76daa94dde38d2315314dae570428e3b6f9
Daniel Andrlik <daniel@andrlik.org>
2019-06-07 09:22:33 -0400

    🎨 Refactor admin utils tests

--- looking_for_group/adminutils/tests/test_views.py
+++ looking_for_group/adminutils/tests/test_views.py
@@ -1,85 +1,90 @@
-from ...gamer_profiles.models import GamerProfile
-from ...gamer_profiles.tests import factories
-from ...games.tests.test_views import AbstractViewTestCaseSignals
-from ...user_preferences.models import Preferences
-from ...users.models import User
+import pytest
+from django.urls import reverse
+
 from ..views import get_filtered_user_queryset
 
-
-class AbstractUtilsTest(AbstractViewTestCaseSignals):
-
-    def setUp(self):
-        super().setUp()
-        self.admin_gamer = factories.GamerProfileFactory()
-        self.admin_gamer.user.is_superuser = True
-        self.admin_gamer.user.save()
-        Preferences.objects.create(gamer=self.gamer1, notification_digest=True)
-        Preferences.objects.create(gamer=self.gamer2, feedback_volunteer=True)
-        for gamer in GamerProfile.objects.exclude(id__in=[self.gamer1.pk, self.gamer2.pk]):
-            Preferences.objects.create(gamer=gamer)
+pytestmark = pytest.mark.django_db(transaction=True)
 
 
-class FilterTest(AbstractUtilsTest):
-
-    def setUp(self):
-        super().setUp()
-
-    def test_filtering_empty(self):
-        assert get_filtered_user_queryset([], 'any').count() == User.objects.count()
-
-    def test_filtering_any(self):
-        assert get_filtered_user_queryset(['notification_digest', 'feedback_volunteer'], "any").count() == 2
-
-    def test_filtering_all(self):
-        assert get_filtered_user_queryset(['notification_digest', 'feedback_volunteer'], "all").count() == 0
-
-    def test_filtering_none(self):
-        assert get_filtered_user_queryset(['notification_digest', 'feedback_volunteer'], "none").count() == User.objects.count() - 2
-
-
-class TestNotificationSend(AbstractUtilsTest):
-
-    def setUp(self):
-        super().setUp()
-        self.view_name = 'adminutils:notification'
-
-    def test_login_required(self):
-        self.assertLoginRequired(self.view_name)
-
-    def test_unauthorized_user(self):
-        with self.login(username=self.gamer1.username):
-            self.get(self.view_name)
-            self.response_403()
-
-    def test_admin_access(self):
-        with self.login(username=self.admin_gamer.username):
-            self.assertGoodView(self.view_name)
-
-    def test_post_notification(self):
-        with self.login(username=self.admin_gamer.username):
-            self.post(self.view_name, data={'message': "Hello, friend", 'filter_options': ['feedback_volunteer'], 'filter_mode': 'any'})
-            self.response_302()
+@pytest.mark.parametrize(
+    "filter_type, filter_mode, expected_result",
+    [
+        ([], "any", 5),  # Find everyone
+        (
+            ["notification_digest", "feedback_volunteer"],
+            "any",
+            2,
+        ),  # Find users with either selected
+        (
+            ["notification_digest", "feedback_volunteer"],
+            "all",
+            0,
+        ),  # Find users with both selected
+        (
+            ["notification_digest", "feedback_volunteer"],
+            "none",
+            3,
+        ),  # Find users where neither is selected.
+    ],
+)
+def test_filtering(admin_testdata, filter_type, filter_mode, expected_result):
+    assert (
+        get_filtered_user_queryset(filter_type, filter_mode).count() == expected_result
+    )
 
 
-class TestEmailSend(AbstractUtilsTest):
+@pytest.mark.parametrize(
+    "view_name, gamer_to_use, expected_status_code, expected_location",
+    [
+        ("adminutils:notification", None, 302, "/accounts/login/"),  # Must be logged in
+        ("adminutils:email", None, 302, "/accounts/login/"),  # Must be logged in
+        ("adminutils:notification", "gamer1", 403, None),  # Must be an admin
+        ("adminutils:email", "gamer1", 403, None),  # Must be an admin
+        ("adminutils:notification", "admin_gamer", 200, None),  # Admin can access
+        ("adminutils:email", "admin_gamer", 200, None),  # Admin can access
+    ],
+)
+def test_get_views(
+    client,
+    django_assert_max_num_queries,
+    admin_testdata,
+    view_name,
+    gamer_to_use,
+    expected_status_code,
+    expected_location,
+):
+    if gamer_to_use:
+        client.force_login(user=getattr(admin_testdata, gamer_to_use).user)
+    with django_assert_max_num_queries(50):
+        response = client.get(reverse(view_name))
+    assert response.status_code == expected_status_code
+    if expected_location:
+        assert expected_location in response["Location"]
 
-    def setUp(self):
-        super().setUp()
-        self.view_name = 'adminutils:email'
 
-    def test_login_required(self):
-        self.assertLoginRequired(self.view_name)
-
-    def test_unauthorized_user(self):
-        with self.login(username=self.gamer1.username):
-            self.get(self.view_name)
-            self.response_403()
-
-    def test_admin_access(self):
-        with self.login(username=self.admin_gamer.username):
-            self.assertGoodView(self.view_name)
-
-    def test_mass_email(self):
-        with self.login(username=self.admin_gamer.username):
-            self.post(self.view_name, data={"subject": "Greetings", "body": "Do you have the time?", "filter_options": ["feedback_volunteer"], "filter_mode": "any"})
-            self.response_302()
+@pytest.mark.parametrize(
+    "view_name, data_to_send",
+    [
+        (
+            "adminutils:notification",
+            {
+                "message": "Hello, friend",
+                "filter_options": ["feedback_volunteer"],
+                "filter_mode": "any",
+            },
+        ),
+        (
+            "adminutils:email",
+            {
+                "subject": "Greetings",
+                "body": "Do you have the time?",
+                "filter_options": ["feedback_volunteer"],
+                "filter_mode": "any",
+            },
+        ),
+    ],
+)
+def test_sending_messages(client, admin_testdata, view_name, data_to_send):
+    client.force_login(user=admin_testdata.admin_gamer.user)
+    response = client.post(reverse(view_name), data=data_to_send)
+    assert response.status_code == 302
