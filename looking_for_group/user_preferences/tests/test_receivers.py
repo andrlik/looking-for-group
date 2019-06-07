3de7650431be53ab821bbfb4106f3c693d9cdf71
Daniel Andrlik <daniel@andrlik.org>
2019-06-07 09:37:17 -0400

    🎨 Refactor user pref receivers tests

--- looking_for_group/user_preferences/tests/test_receivers.py
+++ looking_for_group/user_preferences/tests/test_receivers.py
@@ -1,26 +1,21 @@
-from ...gamer_profiles.models import CommunityMembership, GamerCommunity
-from ...gamer_profiles.tests.test_views import AbstractViewTest
+import pytest
+
+from ...gamer_profiles.models import CommunityMembership
 from .. import models
 
 
-class TestAddMember(AbstractViewTest):
-    """
-    Test that the receiver sets the subscription setting correctly.
-    """
-    def setUp(self):
-        super().setUp()
-        self.pref3 = models.Preferences.objects.create(gamer=self.gamer3)
-        self.gamer3.refresh_from_db()
-
-    def test_with_default(self):
-        self.pref3.community_subscribe_default = True
-        self.pref3.save()
-        assert self.gamer3.preferences.community_subscribe_default
-        self.community1.add_member(self.gamer3)
-        assert CommunityMembership.objects.get(gamer=self.gamer3, community=self.community1).game_notifications
-
-    def test_without_default(self):
-        self.pref3.community_subscribe_default = False
-        self.pref3.save()
-        self.community1.add_member(self.gamer3)
-        assert not CommunityMembership.objects.get(gamer=self.gamer3, community=self.community1).game_notifications
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.parametrize("not_setting", [True, False])
+def test_add_member(game_testdata, not_setting):
+    pref3 = models.Preferences.objects.create(gamer=game_testdata.gamer3)
+    game_testdata.gamer3.refresh_from_db()
+    pref3.community_subscribe_default = not_setting
+    pref3.save()
+    assert game_testdata.gamer3.preferences.community_subscribe_default == not_setting
+    game_testdata.comm2.add_member(game_testdata.gamer3)
+    assert (
+        CommunityMembership.objects.get(
+            gamer=game_testdata.gamer3, community=game_testdata.comm2
+        ).game_notifications
+        == not_setting
+    )
