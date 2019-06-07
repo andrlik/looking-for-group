7f4d5805dbd1995219c0d2067be5d49298ec1a8c
Daniel Andrlik <daniel@andrlik.org>
2019-06-07 08:56:33 -0400

    🎨 Refactor rpgcollections tests
    Fixes #134

--- looking_for_group/rpgcollections/tests/test_views.py
+++ looking_for_group/rpgcollections/tests/test_views.py
@@ -2,157 +2,166 @@ import pytest
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.db.models.signals import post_delete, post_save
+from django.urls import reverse
 from factory.django import mute_signals
 
 from looking_for_group.game_catalog.tests.test_views import AbstractViewTest
 from looking_for_group.gamer_profiles.tests import factories
 from looking_for_group.rpgcollections import models
 
+pytestmark = pytest.mark.django_db(transaction=True)
 
-class AbstractCollectionsTest(AbstractViewTest):
+
+@pytest.mark.parametrize(
+    "view_name, expected_status_code, gamer_to_use, expected_location",
+    [
+        (
+            "rpgcollections:book-detail",
+            302,
+            None,
+            "/accounts/login/",
+        ),  # Non authenticated for book detail
+        (
+            "rpgcollections:book-detail",
+            403,
+            "gamer3",
+            None,
+        ),  # Authenticated, but not connected for book detail
+        ("rpgcollections:book-detail", 200, "gamer2", None),  # Friend can view.
+        ("rpgcollections:book-detail", 200, "gamer1", None),  # Owner can view
+        (
+            "rpgcollections:edit-book",
+            302,
+            None,
+            "/accounts/login/",
+        ),  # Non-authenticated user for edit book
+        (
+            "rpgcollections:edit-book",
+            403,
+            "gamer2",
+            None,
+        ),  # Only the owner should be able to edit
+        ("rpgcollections:edit-book", 200, "gamer1", None),  # Owner can edit
+        (
+            "rpgcollections:remove-book",
+            302,
+            None,
+            "/accounts/login/",
+        ),  # Non authenticated redirects
+        ("rpgcollections:remove-book", 403, "gamer2", None),  # Only owner can delete
+        ("rpgcollections:remove-book", 200, "gamer1", None),  # Owner can delete
+    ],
+)
+def test_get_requests_for_access(
+    client,
+    collection_testdata,
+    django_assert_max_num_queries,
+    view_name,
+    expected_status_code,
+    gamer_to_use,
+    expected_location,
+):
     """
-    Abstract base test for testing collections.
+    Test that making GET requests for the detail views results in the expected result.
     """
-
-    def setUp(self):
-        super().setUp()
-        with mute_signals(post_save):
-            self.gamer1 = factories.GamerProfileFactory()
-            self.gamer2 = factories.GamerProfileFactory()
-            self.comm1 = factories.GamerCommunityFactory(owner=self.gamer1)
-            self.comm1.add_member(self.gamer1, role="admin")
-            self.gamer3 = factories.GamerProfileFactory()
-            self.comm1.add_member(self.gamer2)
-            self.game_lib1 = models.GameLibrary.objects.create(user=self.gamer1.user)
-            self.cypher_collect_1 = models.Book.objects.create(library=self.game_lib1, content_object=self.cypher, in_print=True)
-            self.game_lib2 = models.GameLibrary.objects.create(user=self.gamer2.user)
-            self.game_lib3 = models.GameLibrary.objects.create(user=self.gamer3.user)
+    if gamer_to_use:
+        client.force_login(user=getattr(collection_testdata, gamer_to_use).user)
+    with django_assert_max_num_queries(50):
+        response = client.get(
+            reverse(
+                view_name, kwargs={"book": collection_testdata.cypher_collect_1.slug}
+            )
+        )
+    assert response.status_code == expected_status_code
+    if expected_location:
+        assert expected_location in response["Location"]
 
 
-class TestViewDetail(AbstractCollectionsTest):
-    """
-    Test viewing a collected copy's detail.
-    """
-
-    def setUp(self):
-        super().setUp()
-        self.view_name = "rpgcollections:book-detail"
-        self.url_kwargs = {"book": self.cypher_collect_1.slug}
-
-    def test_login_required(self):
-        self.assertLoginRequired(self.view_name, **self.url_kwargs)
-
-    def test_dont_allow_unconnected(self):
-        with self.login(username=self.gamer3.username):
-            self.get(self.view_name, **self.url_kwargs)
-            self.response_403()
-
-    def test_friend_view(self):
-        with self.login(username=self.gamer2.username):
-            self.assertGoodView(self.view_name, **self.url_kwargs)
-
-    def test_owner_view(self):
-        with self.login(username=self.gamer1.username):
-            self.assertGoodView(self.view_name, **self.url_kwargs)
+def test_update_book_data(client, collection_testdata):
+    client.force_login(user=collection_testdata.gamer1.user)
+    with mute_signals(post_save):
+        response = client.post(
+            reverse(
+                "rpgcollections:edit-book",
+                kwargs={"book": collection_testdata.cypher_collect_1.slug},
+            ),
+            data={
+                "object_id": collection_testdata.cypher_collect_1.content_object.pk,
+                "in_print": 1,
+                "in_pdf": 1,
+            },
+        )
+    assert response.status_code == 302
+    tmpchk = models.Book.objects.get(pk=collection_testdata.cypher_collect_1.pk)
+    assert tmpchk.in_print and tmpchk.in_pdf
 
 
-class TestEditbook(AbstractCollectionsTest):
-    """
-    Test updating a book.
-    """
-
-    def setUp(self):
-        super().setUp()
-        self.view_name = "rpgcollections:edit-book"
-        self.url_kwargs = {"book": self.cypher_collect_1.slug}
-        self.post_data = {"object_id": self.cypher_collect_1.content_object.pk, "in_print": 1, "in_pdf": 1}
-
-    def test_login_required(self):
-        self.assertLoginRequired(self.view_name, **self.url_kwargs)
-
-    def test_unauthorized_user(self):
-        for user in [self.gamer2, self.gamer3]:
-            with self.login(username=user.username):
-                self.get(self.view_name, **self.url_kwargs)
-                self.response_403()
-
-    def test_authorized_user(self):
-        with self.login(username=self.gamer1.username):
-            self.assertGoodView(self.view_name, **self.url_kwargs)
-
-    def test_update_data(self):
-        with mute_signals(post_save):
-            with self.login(username=self.gamer1.username):
-                self.post(self.view_name, data=self.post_data, **self.url_kwargs)
-                tmpcheck = models.Book.objects.get(pk=self.cypher_collect_1.pk)
-                assert tmpcheck.in_print and tmpcheck.in_pdf
+def test_delete_book(client, collection_testdata):
+    client.force_login(user=collection_testdata.gamer1.user)
+    col_count = models.Book.objects.filter(
+        library=collection_testdata.game_lib1
+    ).count()
+    with mute_signals(post_delete):
+        response = client.post(
+            reverse(
+                "rpgcollections:remove-book",
+                kwargs={"book": collection_testdata.cypher_collect_1.slug},
+            ),
+            data={},
+        )
+    assert (
+        col_count
+        - models.Book.objects.filter(library=collection_testdata.game_lib1).count()
+        == 1
+    )
+    with pytest.raises(ObjectDoesNotExist):
+        models.Book.objects.get(pk=collection_testdata.cypher_collect_1.pk)
 
 
-class TestDeleteBook(AbstractCollectionsTest):
-    """
-    Test for deleting from a collection.
-    """
-
-    def setUp(self):
-        super().setUp()
-        self.view_name = "rpgcollections:remove-book"
-        self.url_kwargs = {"book": self.cypher_collect_1.slug}
-
-    def test_login_required(self):
-        self.assertLoginRequired(self.view_name, **self.url_kwargs)
-
-    def test_unauthorized_user(self):
-        for user in [self.gamer2, self.gamer3]:
-            with self.login(username=user.username):
-                self.get(self.view_name, **self.url_kwargs)
-                self.response_403()
-                self.post(self.view_name, data={}, **self.url_kwargs)
-                self.response_403()
-                assert models.Book.objects.get(pk=self.cypher_collect_1.pk)
-
-    def test_authorized_user(self):
-        with mute_signals(post_save, post_delete):
-            with self.login(username=self.gamer1.username):
-                self.assertGoodView(self.view_name, **self.url_kwargs)
-                self.post(self.view_name, data={}, **self.url_kwargs)
-                self.response_302()
-                with transaction.atomic():
-                    with pytest.raises(ObjectDoesNotExist):
-                        models.Book.objects.get(pk=self.cypher_collect_1.pk)
+def test_post_required_for_add(client, collection_testdata):
+    client.force_login(user=collection_testdata.gamer2.user)
+    response = client.get(
+        reverse("rpgcollections:add-book", kwargs={"booktype": "system"})
+    )
+    assert response.status_code == 405
 
 
-class TestAddBook(AbstractCollectionsTest):
-    """
-    Test creating book records.
-    """
-
-    def setUp(self):
-        super().setUp()
-        self.view_name = "rpgcollections:add-book"
-        self.url_kwargs = {"booktype": "system"}
-        self.post_data = {"object_id": self.cypher.pk, "in_pdf": 1}
-
-    def test_post_required(self):
-        with self.login(username=self.gamer2.username):
-            self.get(self.view_name, **self.url_kwargs)
-            self.response_405()
-
-    def test_logged_in_user(self):
-        with self.login(username=self.gamer2.username):
-            with mute_signals(post_save):
-                self.post(self.view_name, data=self.post_data, **self.url_kwargs)
-                self.response_302()
-                assert self.cypher.collected_copies.filter(library=self.game_lib2).count() > 0
-
-    def test_add_when_aready_there(self):
-        with self.login(username=self.gamer1.username):
-            with mute_signals(post_save):
-                self.post(self.view_name, data=self.post_data, **self.url_kwargs)
-                self.response_302()
-                assert self.cypher.collected_copies.filter(library=self.game_lib1).count() == 1
-
-    def test_bad_data(self):
-        book_count = models.Book.objects.filter(library=self.game_lib3).count()
-        with self.login(username=self.gamer3.username):
-            self.post(self.view_name, data={"object_id": self.cypher.pk}, **self.url_kwargs)
-            assert models.Book.objects.filter(library=self.game_lib3).count() == book_count
+@pytest.mark.parametrize(
+    "gamer_to_use, data, expected_status_code, expected_collection_count_diff",
+    [
+        ("gamer2", "good", 302, 1),  # Valid add for a user that doesn't have it yet.
+        (
+            "gamer1",
+            "good",
+            302,
+            0,
+        ),  # Trying to add when already there should have no effect
+        ("gamer2", "bad", 302, 0),  # Using bad data should not add to the library
+    ],
+)
+def test_add_book(
+    client,
+    collection_testdata,
+    gamer_to_use,
+    data,
+    expected_status_code,
+    expected_collection_count_diff,
+):
+    data_dict = {
+        "good": {"object_id": collection_testdata.cypher.pk, "in_print": 1},
+        "bad": {"object_id": collection_testdata.cypher.pk},
+    }
+    gamer = getattr(collection_testdata, gamer_to_use)
+    client.force_login(user=gamer.user)
+    game_lib = models.GameLibrary.objects.get(user=gamer.user)
+    col_count = models.Book.objects.filter(library=game_lib).count()
+    with mute_signals(post_save):
+        response = client.post(
+            reverse("rpgcollections:add-book", kwargs={"booktype": "system"}),
+            data=data_dict[data],
+        )
+    assert response.status_code == expected_status_code
+    assert (
+        models.Book.objects.filter(library=game_lib).count() - col_count
+        == expected_collection_count_diff
+    )
