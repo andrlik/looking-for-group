af0807c25a8a29ecf7d2e64088974c48f864b677
Daniel Andrlik <daniel@andrlik.org>
2019-06-13 13:14:54 -0400

    Alter restart services script

--- .ebextensions/files/99_restart_services.sh
+++ .ebextensions/files/99_restart_services.sh
@@ -2,14 +2,6 @@
 
 set -xe
 
-/usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf status | grep django_q
-if [[ $? ]]; then
-  /usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf restart django_q
-fi
+/opt/elasticbeanstalk/hooks/restartappserver/enact/99restartnginx.sh
 
-/usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf status | grep django | grep -v django_q
-if [[ $? ]]; then
-    /usr/local/bin/supervisorctl -c /opt/python/etc/supervisor.conf restart django
-fi
-
-eventHelper.py --msg "Worker and web server restarted." --severity INFO
+# eventHelper.py --msg "Worker and web server restarted." --severity INFO
