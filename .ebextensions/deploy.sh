7516838e40e3a1a1ba34b4439b901e24aed7ede2
Daniel Andrlik <daniel@andrlik.org>
2019-06-13 15:09:56 -0400

    Config changes based on experiments

--- .ebextensions/deploy.sh
+++ .ebextensions/deploy.sh
@@ -10,22 +10,21 @@ else
   echo "Start non-leader deploy"
 fi
 
-copy_ext $SCRIPT_PATH/files/django.conf /opt/python/etc/django.conf 0755 root root
-copy_ext $SCRIPT_PATH/files/djangoq.conf /opt/python/etc/djangoq.conf 0755 root root
-copy_ext $SCRIPT_PATH/files/launch_worker /opt/python/bin/launch_worker 0755 root root
+#copy_ext $SCRIPT_PATH/files/django.conf /opt/python/etc/django.conf 0755 root root
+#copy_ext $SCRIPT_PATH/files/launch_worker /opt/python/bin/launch_worker 0755 root root
 
-copy_ext $SCRIPT_PATH/files/99_restart_services.sh /opt/elasticbeanstalk/hooks/appdeploy/enact/99_restart_services.sh 0755 root root
-copy_ext $SCRIPT_PATH/files/99_restart_services.sh /opt/elasticbeanstalk/hooks/configdeploy/enact/99_restart_services.sh 0755 root root
-copy_ext $SCRIPT_PATH/files/99_restart_services.sh /opt/elasticbeanstalk/hooks/restartappserver/enact/99_restart_services.sh 0755 root root
+#copy_ext $SCRIPT_PATH/files/99_restart_services.sh /opt/elasticbeanstalk/hooks/appdeploy/enact/99_restart_services.sh 0755 root root
+#copy_ext $SCRIPT_PATH/files/99_restart_services.sh /opt/elasticbeanstalk/hooks/configdeploy/enact/99_restart_services.sh 0755 root root
+#copy_ext $SCRIPT_PATH/files/99_restart_services.sh /opt/elasticbeanstalk/hooks/restartappserver/enact/99_restart_services.sh 0755 root root
 
 
-script_add_line /opt/python/etc/supervisord.conf "include" "[include]"
-script_add_line /opt/python/etc/supervisord.conf "djangoq.conf" "files=djangoq.conf"
+#script_add_line /opt/python/etc/supervisord.conf "include" "[include]"
+#script_add_line /opt/python/etc/supervisord.conf "djangoq.conf" "files=djangoq.conf"
 #script_add_line /opt/python/etc/supervisord.conf "include" "[include]"
 #script_add_line /opt/python/etc/supervisord.conf "django.conf" "files=django.conf"
 
-supervisorctl -c /opt/python/etc/supervisord.conf reread
+#supervisorctl -c /opt/python/etc/supervisord.conf reread
 
-supervisorctl -c /opt/python/etc/supervisord.conf update
+#supervisorctl -c /opt/python/etc/supervisord.conf reload
 
-supervisorctl -c /opt/python/etc/supervisord.conf restart django_q
+#supervisorctl -c /opt/python/etc/supervisord.conf restart django_q
