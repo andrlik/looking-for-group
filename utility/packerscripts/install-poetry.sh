e043d61ef320f145f7ed4dc0115c2dc42e88ee7f
Daniel Andrlik <daniel@andrlik.org>
2019-08-05 20:27:59 -0400

    Another try

--- utility/packerscripts/install-poetry.sh
+++ utility/packerscripts/install-poetry.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-echo "First installing pyenv for www-data..."
+echo "First installing pyenv..."
 
-sudo -u www-data curl https://pyenv.run | bash
-
-sudo -u www-data echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
-
-chgrp -R www-data /home/ubuntu/.pyenv
-
-sudo -u www-data /home/ubuntu/.pyenv/bin/pyenv update
-
-sudo -u www-data /home/ubuntu/.pyenv/bin/pyenv install 3.7.0
-
-sudo -u www-data /home/ubuntu/.pyenv/bin/pyenv global 3.7.0
+curl https://pyenv.run | bash
+PATH="/home/ubuntu/.pyenv/bin:$PATH"
+export $PATH
+eval "$(pyenv init  -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv update
+pyenv install 3.7.0
+pyenv global 3.7.0
+curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 
 echo "Installing poetry for python3.7..."
 # Install poetry for python3.7
-sudo -u www-data curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
+curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 
 exit 0
