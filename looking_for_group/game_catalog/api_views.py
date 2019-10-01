012f2a8d120c99c1639b4038da76bb3e490d5467
Daniel Andrlik <daniel@andrlik.org>
2019-10-01 17:43:41 -0400

    🚧 Starting on dynamic form

--- looking_for_group/game_catalog/api_views.py
+++ looking_for_group/game_catalog/api_views.py
@@ -1,14 +1,16 @@
+# Note, we don't provide create, edit, or delete views for these now as we'll handle those via the admin.
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from . import models, serializers
-
-# Note, we don't provide create, edit, or delete views for these now as we'll handle those via the admin.
 
 
 class GamePublisherViewSet(viewsets.ReadOnlyModelViewSet):
     """
     List and detail views for :class:`game_catalog.models.GamePublisher`.
     """
+
+    filter_backends = [DjangoFilterBackend]
 
     serializer_class = serializers.GamerPublisherSerializer
     queryset = models.GamePublisher.objects.all().prefetch_related(
@@ -21,6 +23,8 @@ class GameSystemViewSet(viewsets.ReadOnlyModelViewSet):
     Provides list and details for :class:`game_catalog.models.GameSystem`.
     """
 
+    filter_backends = [DjangoFilterBackend]
+
     serializer_class = serializers.GameSystemSerializer
 
     queryset = (
@@ -30,16 +34,40 @@ class GameSystemViewSet(viewsets.ReadOnlyModelViewSet):
     )
 
 
+class GameEditionViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Provides list and detail view for :class:`game_catalog.models.GameEdition`.
+    """
+
+    filter_backends = [DjangoFilterBackend]
+    serializer_class = serializers.GameEditionSerializer
+    queryset = (
+        models.GameEdition.objects.all()
+        .select_related("publisher")
+        .prefetch_related("publishedmodule_set")
+    )
+
+
+class SourcebookViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Provides list and detail view for sourcebooks.
+    """
+
+    filter_backends = [DjangoFilterBackend]
+    serializer_class = serializers.SourcebookSerializer
+    queryset = models.SourceBook.objects.all().select_related("publisher", "edition")
+
+
 class PublishedGameViewSet(viewsets.ReadOnlyModelViewSet):
     """
     Provides list and detail view for :class:`game_catalog.models.PublishedGame`.
     """
 
+    filter_backends = [DjangoFilterBackend]
+
     serializer_class = serializers.PublishedGamerSerializer
 
-    queryset = (
-        models.PublishedGame.objects.all().prefetch_related("editions")
-    )
+    queryset = models.PublishedGame.objects.all().prefetch_related("editions")
 
 
 class PublishedModuleViewSet(viewsets.ReadOnlyModelViewSet):
@@ -47,8 +75,13 @@ class PublishedModuleViewSet(viewsets.ReadOnlyModelViewSet):
     Provides list and detail views for :class:`game_catalog.models.PublishedModule`.
     """
 
+    filter_backends = [DjangoFilterBackend]
+
     serializer_class = serializers.PublishedModuleSerializer
 
     queryset = models.PublishedModule.objects.all().select_related(
-        "publisher", "parent_game_edition", "parent_game_edition__game", "parent_game_edition__game_system"
+        "publisher",
+        "parent_game_edition",
+        "parent_game_edition__game",
+        "parent_game_edition__game_system",
     )
