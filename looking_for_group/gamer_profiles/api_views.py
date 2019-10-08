b5bfee1a679869d734a5ecd0f8221ae0202b7df8
Daniel Andrlik <daniel@andrlik.org>
2019-10-08 17:44:43 -0400

    🚧 Adding permissions to social api views

--- looking_for_group/gamer_profiles/api_views.py
+++ looking_for_group/gamer_profiles/api_views.py
@@ -20,22 +20,38 @@ class GamerCommunityViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
     queryset = models.GamerCommunity.objects.all()
     serializer_class = serializers.GamerCommunitySerializer
 
-    @action_permission_required('community.delete_community')
+    @action(methods=["post"], permission_required="community.apply")
+    def apply(self, request, pk=None):
+        """
+        Apply to this community.
+        """
+        message = None
+        if "message" in request.data.keys():
+            message = request.data["message"]
+        application = models.CommunityApplication.objects.create(
+            gamer=request.user.gamerprofile,
+            community=self.get_object(),
+            message=message,
+            status="review",
+        )
+        return status.HTTP_201_CREATED
+
+    @action_permission_required("community.delete_community")
     def destroy(self, request, pk=None):
-        '''
+        """
         Destroy the community.
-        '''
+        """
         return super().destroy(request, pk)
 
-    @action_permission_required('community.edit_community')
+    @action_permission_required("community.edit_community")
     def update(self, request, pk=None):
         return super().update(request, pk)
 
-    @action_permission_required('community.edit_community')
+    @action_permission_required("community.edit_community")
     def partial_update(self, request, pk=None):
         return super().partial_update(request, pk)
 
-    @action(methods=["get"], detail=True, permission_required='community.view_details')
+    @action(methods=["get"], detail=True, permission_required="community.view_details")
     def list_members(self, request, pk=None):
         """
         List members of a community.
@@ -44,6 +60,7 @@ class GamerCommunityViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
             models.CommunityMembership.objects.filter(community=self.get_object())
             .select_related("gamer")
             .prefetch_related("gamer__user")
+            .filter(gamer__user=request.user)
         )
         page = self.paginate_queryset(member_queryset)
         if page is not None:
@@ -54,7 +71,7 @@ class GamerCommunityViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
         )
         return Response(member_serials.data, status=status.HTTP_200_OK)
 
-    @action(methods=["get"], detail=True, permission_required='community.ban_user')
+    @action(methods=["get"], detail=True, permission_required="community.ban_user")
     def list_banned_users(self, request, pk=None):
         """
         Retrieve a list of banned users for this community.
@@ -63,7 +80,7 @@ class GamerCommunityViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
         serializer = serializers.BannedUserSerializer(ban_list, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(methods=["get"], detail=True, permission_required='community.kick_user')
+    @action(methods=["get"], detail=True, permission_required="community.kick_user")
     def list_kicked_users(self, request, pk=None):
         """
         List users that have been kicked/suspended from community.
@@ -72,7 +89,7 @@ class GamerCommunityViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
         serializer = serializers.KickedUserSerializer(kick_list, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(methods=["get"], detail=True, permission_required='community.view_details')
+    @action(methods=["get"], detail=True, permission_required="community.view_details")
     def list_admins(self, request, pk=None):
         """
         List community admins.
@@ -81,7 +98,7 @@ class GamerCommunityViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
         serializer = serializers.GamerProfileSerializer(admins, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(methods=["get"], detail=True, permission_required='community.view_details')
+    @action(methods=["get"], detail=True, permission_required="community.view_details")
     def list_mods(self, request, pk=None):
         """
         List community moderators.
@@ -154,9 +171,34 @@ class GamerProfileViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
 
     permission_classes = (permissions.IsAuthenticated,)
     permission_required = "community.list_communities"
-    object_permission_required = 'profile.view_detail'
+    object_permission_required = "profile.view_detail"
     serializer_class = serializers.GamerProfileSerializer
-    queryset = models.GamerProfile.objects.all()
+    # queryset = models.GamerProfile.objects.all().select_related("user").prefetch_related("friends")
+
+    def get_queryset(self):
+        qs_public = models.GamerProfile.objects.filter(private=False).select_related(
+            "user"
+        )
+        qs_friends = self.request.user.gamerprofile.friends.all()
+        qs_community = models.GamerProfile.objects.filter(
+            id__in=[
+                cm.gamer.id
+                for cm in models.CommunityMembership.objects.filter(
+                    community__in=self.request.user.gamerprofile.communities.all()
+                )
+            ]
+        )
+        qs_pub_friends = qs_public.union(qs_friends)
+        qs_combined = qs.union(qs_community)
+        qs_remove_blocked = combined_qs.exclude(
+            id__in=[
+                b.blocker.id
+                for b in models.BlockedUser.objects.filter(
+                    blockee=request.user.gamerprofile
+                )
+            ]
+        )
+        return qs_remove_blocked
 
     # @action_permission_required(
     #     "profile.view_note",
@@ -180,7 +222,11 @@ class GamerNoteViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
 
     permission_required = "community.edit_profile_note"
     serializer_class = serializers.GamerNoteSerializer
-    queryset = models.GamerNote.objects.all().select_related("author", "gamer")
+
+    def get_queryset(self):
+        return models.GamerNote.objects.filter(
+            author=self.request.user.gamerprofile
+        ).select_related("author", "gamer", "gamer__user")
 
 
 class BlockedUserViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
@@ -189,10 +235,12 @@ class BlockedUserViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
     """
 
     permission_required = "community.view_blocked_user"
-    queryset = models.BlockedUser.objects.all().select_related(
-        "blocker", "blocked_user"
-    )
     serializer_class = serializers.BlockedUserSerializer
+
+    def get_queryset(self):
+        models.BlockedUser.objects.filter(
+            blocker=self.request.user.gamerprofile
+        ).select_related("blocker", "blocked_user")
 
 
 class MutedUserViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
@@ -201,21 +249,64 @@ class MutedUserViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
     """
 
     permission_required = "community.view_muted_user"
-    queryset = models.MutedUser.objects.all().select_related("muter", "muted_user")
     serializer_class = serializers.MuteduserSerializer
+
+    def get_queryset(self):
+        models.MutedUser.objects.filter(
+            muter=self.request.user.gamerprofile
+        ).select_related("muter", "muted_user")
 
 
 class CommunityApplicationViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
     """
-    View set for community applications.
+    Viewset for a user reviewing their own applications.
+    """
+
+    permission_required = "community.view_communities"
+    object_permission_required = "commuity.edit_application"
+
+    def get_queryset(self):
+        return models.CommunityApplication.objects.filter(
+            gamer=self.request.user.gamerprofile
+        ).select_related("community")
+
+    @action_permission_required("community.apply")
+    def create(self, request, *args, **kwargs):
+        """
+        Not permitted in this view set. Use the community apply action instead.
+        """
+        return Response(status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Not allowed. You need to withdraw the application and resumbit if you want to change something.
+        """
+        return Response(status.HTTP_403_FORBIDDEN)
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+        Not allowed. You need to withdraw the application and resumbit if you want to change something.
+        """
+        return Response(status.HTTP_403_FORBIDDEN)
+
+
+class CommunityAdminApplicationViewSet(PermissionRequiredMixin, viewsets.ModelViewSet):
+    """
+    View set for reviewing approving/rejecting community applications.
     """
 
     permission_required = "community.view_communities"
     object_permission_required = "community.destroy_application"
-    queryset = models.CommunityApplication.objects.all().select_related(
-        "community", "gamer"
-    )
     serializer_class = serializers.CommunityApplicationSerializer
+
+    def get_queryset(self):
+        models.CommunityApplication.objects.filter(
+            community__in=models.CommunityApplication.objects.filter(
+                gamer=self.request.user.gamerprofile,
+                community_role="admin",
+                status="review",
+            )
+        ).select_related("community", "gamer")
 
     @action_permission_required("community.approve_application")
     @action(methods=["post"], detail=True)
